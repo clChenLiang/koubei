500ffac071b0b17f1a6fbe3768e09a92adae3c90
clChenLiang <clchenliang@hotmail.com>
2017-09-05 11:21:05 +0800

    更改bug

--- stuCard/public/javascripts/cl_stuAdmin.js
+++ stuCard/public/javascripts/cl_stuAdmin.js
@@ -3,20 +3,28 @@
  */
 DEBUG = false;
 $(document).ready(function(){
-    var currentPage = 1;
+    var currentPage = {
+        getStus:1,
+        getConsume:1
+    };
     $('.menu .item').tab();
     // 绑定事件
     // 使用父元素监听；学生总览和消费记录
     $("tfoot div").click(function(event){
         DEBUG ? ( console.log("click",event.target.text,event.target.innerText,event)) : null;
+        // 可以使用多种方式实现：在按钮btn 中增加 stuinfo  consume class进行区分
+        // 此处，出于统一与练习 event.target.parentElement 目的，保存之; 分别表示 i a 标签触发上去
+        var temp = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        var action = $(temp).attr('data-tab') || $(temp.parentElement).attr('data-tab');
+        action = action === "first" ? "getStus" : "getConsume";
         var conditions = {};
         if(event.target.text){
             // 此处有 ... 在后续过程需要处理
-            currentPage = parseInt(event.target.text)||currentPage;
+            currentPage[action] = parseInt(event.target.text)||currentPage[action];
         }else{
             // 判断点击的是下一页还是上一页;并确保首页为 1
-            event.target.className.indexOf("right") > -1 ? currentPage++ : currentPage--;
-            currentPage = currentPage > 0 ? currentPage : 1;
+            event.target.className.indexOf("right") > -1 ? currentPage[action]++ : currentPage[action]--;
+            currentPage[action] = currentPage[action] > 0 ? currentPage[action] : 1;
         }
         var formstu = parseInt($($(".searchSpe").parent()[0]).find("select").val());
         // NaN 判断
@@ -25,15 +33,11 @@ $(document).ready(function(){
         }
 
 
-        DEBUG ? ( console.log(currentPage)) : null;
-        // 可以使用多种方式实现：在按钮btn 中增加 stuinfo  consume class进行区分
-        // 此处，出于统一与练习 event.target.parentElement 目的，保存之; 分别表示 i a 标签触发上去
-        var temp = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
-        var action = $(temp).attr('data-tab') || $(temp.parentElement).attr('data-tab');
-        action = action === "first" ? "getStus" : "getConsume";
+        DEBUG ? ( console.log(currentPage[action])) : null;
+
         DEBUG ? ( console.log(action)) : null;
         // 暂定每页放5条数据
-        getData(action,{pages:currentPage,limit:5,conditions:JSON.stringify( conditions )},function(rows){
+        getData(action,{pages:currentPage[action],limit:5,conditions:JSON.stringify( conditions )},function(rows){
             $(temp).find("tbody").html( generatTable(rows) );
         });
         //getData("getStus",{pages:currentPage,skip:5},function(a){alert(JSON.stringify(a))});
@@ -54,21 +58,25 @@ $(document).ready(function(){
             case 0:// 新增卡
                 getData("addStu",data,function(a){
                     DEBUG ? ( console.log("addStu 成功")) : null;
+                    alert("添加新用户成功！");
                 });
                 break;
             case 1:// 添加消费记录
                 getData("addStuConsume",data,function(a){
                     DEBUG ? ( console.log("addStuConsume 成功")) : null;
+                    alert("添加消费记录成功！");
                 });
                 break;
             case 2:// 更改信息
                 getData("updateStuCard",data,function(a){
                     DEBUG ? ( console.log("updateStuCard 成功")) : null;
+                    alert("更新用户信息成功！")
                 });
                 break;
             case 3:// 删除卡
                 getData("delCard",data,function(a){
                     DEBUG ? ( console.log("delCard 成功")) : null;
+                    alert("删除用户成功！")
                 });
                 break;
             default :
@@ -76,6 +84,13 @@ $(document).ready(function(){
         }
     })
     // 页面初始化，切换
+    function initRecord(){
+        getData("getStus",{pages:currentPage["getStus"],limit:5,conditions:{}},function(rows){
+            DEBUG ? ( console.log("rows; ",rows)) : null;
+            $(".tab.active").find("tbody").html( generatTable(rows) );
+        });
+    }
+    initRecord();
     $(".top>a.item").click(function(e){
         DEBUG ? ( console.log(e.target.text)) : null;
         DEBUG ? ( console.log(e.target.dataset.tab)) : null;
@@ -83,42 +98,87 @@ $(document).ready(function(){
         switch ( e.target.dataset.tab ){
             // 使用中文
             case "first"://"学生总览":
-                getData("getStus",{pages:1,limit:5,conditions:{}},function(rows){
-                    DEBUG ? ( console.log("rows; ",rows)) : null;
-                    $(".tab.active").find("tbody").html( generatTable(rows) );
-                });
+                //if(!initRecord["first"]){
+                    getData("getStus",{pages:currentPage["getStus"],limit:5,conditions:{}},function(rows){
+                        DEBUG ? ( console.log("rows; ",rows)) : null;
+                        $(".tab.active").find("tbody").html( generatTable(rows) );
+                    });
+                //    initRecord["first"] = true;
+                //}
+
                 break;
             case "second"://"消费记录":
-                getData("getStus",{wants:"stuNum"},function(rows){
-                    rows = [{stuNum:"请选择要查询的学生"}].concat(rows);
-                    DEBUG ? ( console.log("rows; ",rows)) : null;
-                    $(".tab.active").find("[name='fromStu']").html( generatOpts(rows) );
-                });
-                getData("getConsume",{pages:1,limit:5},function(rows){
-                    DEBUG ? ( console.log("rows; ",rows)) : null;
-                    $(".tab.active").find("tbody").html( generatTable(rows) );
-                });
+                //if(!initRecord["second"]){
+                    getData("getStus",{wants:"stuNum"},function(rows){
+                        rows = [{stuNum:"请选择要查询的学生"}].concat(rows);
+                        DEBUG ? ( console.log("rows; ",rows)) : null;
+                        $(".tab.active").find("[name='fromStu']").html( generatOpts(rows) );
+                    });
+                    getData("getConsume",{pages:currentPage["getConsume"],limit:5},function(rows){
+                        DEBUG ? ( console.log("rows; ",rows)) : null;
+                        $(".tab.active").find("tbody").html( generatTable(rows) );
+                    });
+                //    initRecord["second"] = true;
+                //}
+
                 break;
             case "third"://"新增办卡":
                 break;
             case "four"://"模拟消费":
-                getData("getStus",{wants:"stuNum"},function(rows){
-                    DEBUG ? ( console.log("rows; ",rows)) : null;
-                    // 只需要id
-                    $(".tab.active").find("[name='fromStu']").html( generatOpts(rows) );
-                });
+
+                //if(!initRecord["four"]){
+                    getData("getStus",{wants:"stuNum"},function(rows){
+                        DEBUG ? ( console.log("rows; ",rows)) : null;
+                        // 只需要id
+                        $(".tab.active").find("[name='fromStu']").html( generatOpts(rows) );
+                    });
+                    //initRecord["four"] = true;
+                //}
+
                 break;
             case "five"://"修改信息":
-                getData("getStus",{wants:"stuNum"},function(rows){
-                    DEBUG ? ( console.log("rows; ",rows)) : null;
-                    // 只需要id
-                    $(".tab.active").find("[name='stuNum']").html( generatOpts(rows) );
-                });
+
+                //if(!initRecord["five"]){
+                    getData("getStus",{wants:"stuNum"},function(rows){
+                        DEBUG ? ( console.log("rows; ",rows)) : null;
+                        // 只需要id
+                        $(".tab.active").find("[name='stuNum']").html( generatOpts(rows) );
+                    });
+                //    initRecord["five"] = true;
+                //}
+
                 break;
             default :
                 break;
         }
     })
+
+
+    /*function initTab(){
+        getData("getStus",{pages:1,limit:5,conditions:{}},function(rows){
+            DEBUG ? ( console.log("rows; ",rows)) : null;
+            $(".tab.active").find("tbody").html( generatTable(rows) );
+        });
+        getData("getStus",{wants:"stuNum"},function(rows){
+            rows = [{stuNum:"请选择要查询的学生"}].concat(rows);
+            DEBUG ? ( console.log("rows; ",rows)) : null;
+            $(".tab.active").find("[name='fromStu']").html( generatOpts(rows) );
+        });
+        getData("getConsume",{pages:1,limit:5},function(rows){
+            DEBUG ? ( console.log("rows; ",rows)) : null;
+            $(".tab.active").find("tbody").html( generatTable(rows) );
+        });
+        getData("getStus",{wants:"stuNum"},function(rows){
+            DEBUG ? ( console.log("rows; ",rows)) : null;
+            // 只需要id
+            $(".tab.active").find("[name='fromStu']").html( generatOpts(rows) );
+        });
+        getData("getStus",{wants:"stuNum"},function(rows){
+            DEBUG ? ( console.log("rows; ",rows)) : null;
+            $(".tab.active").find("[name='stuNum']").html( generatOpts(rows) );
+        });
+    }
+    initTab();*/
 
     // 给选项框添加onchange事件
     $("select[name='stuNum']").change(function(e){
@@ -136,7 +196,7 @@ $(document).ready(function(){
     $(".searchSpe").click(function(e){
         DEBUG ? ( console.log()) : null;
         var formstu = $($(this).parent()[0]).find("select").val();
-        getData("getConsume",{pages:1,limit:5,conditions:JSON.stringify({fromStu:[ parseInt(formstu) ]})},function(rows){
+        getData("getConsume",{pages:currentPage["getConsume"],limit:5,conditions:JSON.stringify({fromStu:[ parseInt(formstu) ]})},function(rows){
             DEBUG ? ( console.log("rows; ",rows)) : null;
             $(".tab.active").find("tbody").html( generatTable(rows) );
         });
